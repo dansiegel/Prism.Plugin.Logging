a63c4a0118a54ec2caec3ae1dcbeb63d12f3e593
Dan Siegel <me@dansiegel.net>
2017-08-03 19:21:58 -0700

    Adding Sample

---
+++ sample/LoggingDemo/ConsoleUtility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace LoggingDemo
+{
+    public static class ConsoleUtility
+    {
+        public const string UNKNOWN = "unknown";
+
+        public static string Option(string question, params string[] options)
+        {
+            if(options == null || options.Length == 0) return UNKNOWN;
+
+            WriteLine(question);
+            for(int i = 1; i <= options.Length; i++)
+            {
+                WriteLine($"{i}) {options[i-1]}");
+            }
+            WriteLine();
+            WriteLine("Select the number of the option you wish to choose...");
+            if(int.TryParse(ReadLine(), out int result) && result <= options.Length)
+            {
+                return options[result - 1];
+            }
+
+            return UNKNOWN;
+        }
+
+        public static string Question(string question)
+        {
+            WriteLine(question);
+            return ReadLine();
+        }
+
+        public static T Question<T>(string question)
+        {
+            return (T)Convert.ChangeType(Question(question), typeof(T));
+        }
+    }
+}
---
+++ sample/LoggingDemo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Prism.Logging;
+using Prism.Logging.Loggly;
+using Prism.Logging.Syslog;
+
+namespace LoggingDemo
+{
+    class Program
+    {
+        static bool Continue = true;
+        const string Generic = "Generic Syslog";
+        const string LogglySyslog = "Loggly Syslog";
+        const string LogglyHttp = "Loggly Http";
+
+        static void Main(string[] args)
+        {
+            var logger = GetLogger();
+            while(Continue)
+            {
+                Console.Clear();
+                string message = ConsoleUtility.Question("Enter a test message, or type 'quit' to exit:");
+                if(message.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                logger.Log(message, Category.Debug, Priority.None);
+            }
+
+            Console.WriteLine("Thanks for logging");
+        }
+
+        private static ILoggerFacade GetLogger()
+        {
+            switch(ConsoleUtility.Option("Which Logger would you like to use?", Generic, LogglySyslog, LogglyHttp, "Quit"))
+            {
+                case Generic:
+                    var genOptions = new Options
+                    {
+                        HostNameOrIp = ConsoleUtility.Question("What is the Host Name or IP of your Syslog Server?"),
+                        Port = ConsoleUtility.Question<int>("What is the port your Syslog Server is listening on?"),
+                        AppNameOrTag = "LoggingDemo"
+                    };
+
+                    return new SyslogLogger(genOptions);
+                case LogglySyslog:
+                    return new LogglySyslogLogger(GetLogglyOptions());
+                case LogglyHttp:
+                    return new LogglyHttpLogger(GetLogglyOptions());
+                case "Quit":
+                    break;
+                default:
+                    Console.WriteLine("You selected a bad option");
+                    break;
+            }
+
+            Continue = false;
+            return null;
+        }
+
+        private static ILogglyOptions GetLogglyOptions() =>
+            new LogglyOptions
+            {
+                AppName = "LoggingDemo",
+                Token = ConsoleUtility.Question("What is your Loggly Token?")
+            };
+    }
+
+    public class Options : ISyslogOptions
+    {
+        public string HostNameOrIp { get; set; }
+
+        public int? Port { get; set; }
+
+        public string AppNameOrTag { get; set; }
+    }
+
+    public class LogglyOptions : ILogglyOptions
+    {
+        public string Token { get; set; }
+
+        public string AppName { get; set; }
+
+        public IEnumerable<string> Tags => new string[]
+        {
+            "http",
+            "test"
+        };
+    }
+}
