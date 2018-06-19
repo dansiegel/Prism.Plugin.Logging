2465e4c48eeec94cbafa91d01ab2146e710708fb
Dan Siegel <me@dansiegel.net>
2018-06-18 22:21:02 -0700

    update for ILogger

--- src/Prism.Plugin.Logging.Loggly/LogglyHttpLogger.cs
+++ src/Prism.Plugin.Logging.Loggly/LogglyHttpLogger.cs
@@ -1,14 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Prism.Logging.Http;
 
 namespace Prism.Logging.Loggly
 {
-    public class LogglyHttpLogger : HttpLogger, ILoggerFacade
+    public class LogglyHttpLogger : HttpLogger, ILogger
     {
         protected const string LogglyUriTemplate = "{0}/inputs/{1}/tag/{2}/";
 
@@ -27,25 +24,60 @@ namespace Prism.Logging.Loggly
                 Priority = priority,
                 Category = category,
                 Message = message
-            }, LogglyUri()).ContinueWith(t => { });
+            }, LogglyUri(Tags())).ContinueWith(t => { });
         }
 
         protected virtual string LogglyBaseUri =>
             "https://logs-01.loggly.com";
 
-        protected virtual Uri LogglyUri() =>
-            new Uri(string.Format(LogglyUriTemplate, LogglyBaseUri, _options.Token, Tags()));
+        protected virtual Uri LogglyUri(string tags) =>
+            new Uri(string.Format(LogglyUriTemplate, LogglyBaseUri, _options.Token, tags));
 
-        protected virtual string Tags()
-        {
-            var tags = new List<string>{
-                WebUtility.UrlEncode(_options.AppName)
-            };
-            foreach (var tag in _options.Tags)
+        protected IList<string> DefaultTags() =>
+            new List<string>(_options.Tags)
             {
-                tags.Add(WebUtility.UrlEncode(tag));
+                _options.AppName
+            };
+
+        protected virtual string Tags() => 
+            Tags(DefaultTags());
+
+        protected virtual string Tags(IList<string> tags)
+        {
+            for(var i = 0; i < tags.Count; i++)
+            {
+                tags[i] = WebUtility.UrlEncode(tags[i]);
             }
+
             return string.Join(",", tags);
+        }
+
+        public void Log(string message, IDictionary<string, string> properties)
+        {
+            if(properties == null)
+            {
+                properties = new Dictionary<string, string>();
+            }
+
+            properties.Add("HostName", Dns.GetHostName());
+            properties.Add("Message", message);
+
+            PostMessageAsync(properties, LogglyUri(Tags())).ContinueWith(t => { });
+        }
+
+        public void Report(Exception ex, IDictionary<string, string> properties)
+        {
+            if (properties == null)
+            {
+                properties = new Dictionary<string, string>();
+            }
+
+            properties.Add("HostName", Dns.GetHostName());
+            properties.Add("Type", ex.GetType().FullName);
+            properties.Add("Message", ex.Message);
+            properties.Add("StackTrace", ex.StackTrace);
+
+            PostMessageAsync(properties, LogglyUri(Tags())).ContinueWith(t => { });
         }
     }
 }
