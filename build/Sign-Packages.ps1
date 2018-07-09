0b4cfe2329430d49f84b433cd7b2746487808fe9
Dan Siegel <me@dansiegel.net>
2018-07-08 21:43:45 -0700

    update signing script

--- build/Sign-Packages.ps1
+++ build/Sign-Packages.ps1
@@ -6,19 +6,19 @@ if([string]::IsNullOrEmpty($env:SignClientSecret)){
     return;
 }
 
-dotnet tool install --tool-path . SignClient
+dotnet tool install SignClient -g
 
 # Setup Variables we need to pass into the sign client tool
 
 $appSettings = "$currentDirectory\appsettings.json"
 $fileList = "$currentDirectory\filelist.txt"
 
-$nupkgs = gci $env:BUILD_ARTIFACTSTAGINGDIRECTORY\*.nupkg -recurse | Select-Object -ExpandProperty FullName
+$nupkgs = gci $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Prism.*.nupkg -recurse | Select-Object -ExpandProperty FullName
 
 foreach ($nupkg in $nupkgs){
    Write-Host "Submitting $nupkg for signing"
 
-    .\SignClient 'sign' -c $appSettings -i $nupkg -f $fileList -r $env:SignClientUser -s $env:SignClientSecret -n 'Prism' -d 'Prism' -u 'https://github.com/PrismLibrary/Prism' 
+    SignClient 'sign' -c $appSettings -i $nupkg -f $fileList -r $SignClientUser -s $SignClientSecret -n 'Prism.Plugin.Logging' -d 'Prism.Plugin.Logging' -u 'https://github.com/dansiegel/Prism.Plugin.Logging' 
 
     Write-Host "Finished signing $nupkg"
 }
