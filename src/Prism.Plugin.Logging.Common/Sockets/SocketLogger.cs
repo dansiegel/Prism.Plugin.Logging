b6963f4139b4eeb42ae45eb97427de33e3290eee
Dan Siegel <me@dansiegel.net>
2019-02-09 14:37:49 -0800

    Make SocketLogger implement ILogger

--- src/Prism.Plugin.Logging.Common/Sockets/SocketLogger.cs
+++ src/Prism.Plugin.Logging.Common/Sockets/SocketLogger.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 
 namespace Prism.Logging.Sockets
 {
-    public class SocketLogger : SocketMessenger, ILoggerFacade
+    public class SocketLogger : SocketMessenger, ILogger
     {
         private ISocketLoggerOptions _options { get; }
 
@@ -28,6 +31,42 @@ namespace Prism.Logging.Sockets
 
         private void SendMessage(string message) =>
             SendMessage(new BasicMessage { Message = message }, _options.HostOrIp, _options.Port);
+
+        public void Log(string message, IDictionary<string, string> properties)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine(message);
+            AppendProperties(builder, properties);
+            SendMessage(builder.ToString());
+        }
+
+        public void TrackEvent(string name, IDictionary<string, string> properties)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($"Tracked Event: {name}");
+            AppendProperties(builder, properties);
+            SendMessage(builder.ToString());
+        }
+
+        public void Report(Exception ex, IDictionary<string, string> properties)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($"Reported Exception:");
+            builder.AppendLine(ex.ToString());
+            AppendProperties(builder, properties);
+            SendMessage(builder.ToString());
+        }
+
+        private void AppendProperties(StringBuilder builder, IDictionary<string, string> properties)
+        {
+            if (properties != null || !properties.Any()) return;
+
+            builder.AppendLine("  Properties:");
+            foreach(var prop in properties)
+            {
+                builder.AppendLine($"    {prop.Key}: {prop.Value}");
+            }
+        }
 
         private class BasicMessage : ILogMessage
         {
