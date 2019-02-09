1db14afd94000a15d19c963ae24eabbf82ce7562
Dan Siegel <me@dansiegel.net>
2019-02-09 14:37:24 -0800

    adding Tuple support for logging methods

--- src/Prism.Plugin.Logging.Abstractions/ILoggerExtensions.cs
+++ src/Prism.Plugin.Logging.Abstractions/ILoggerExtensions.cs
@@ -8,7 +8,7 @@ namespace Prism.Logging
     {
         public static void Debug(this ILogger logger, string message, IDictionary<string, string> properties = null)
         {
-            if (properties == null)
+            if (properties is null)
             {
                 properties = new Dictionary<string, string>();
             }
@@ -18,9 +18,12 @@ namespace Prism.Logging
             logger.Log(message, properties);
         }
 
+        public static void Debug(this ILogger logger, string message, params (string key, string value)[] properties) =>
+            Debug(logger, message, GetProperties(properties));
+
         public static void Info(this ILogger logger, string message, IDictionary<string, string> properties = null)
         {
-            if (properties == null)
+            if (properties is null)
             {
                 properties = new Dictionary<string, string>();
             }
@@ -29,6 +32,9 @@ namespace Prism.Logging
 
             logger.Log(message, properties);
         }
+
+        public static void Info(this ILogger logger, string message, params (string key, string value)[] properties) =>
+            Info(logger, message, GetProperties(properties));
 
         public static void Log(this ILogger logger, string message) =>
             logger.Log(message, null);
@@ -46,6 +52,9 @@ namespace Prism.Logging
             logger.Log(ex.Message, properties);
         }
 
+        public static void Log(this ILogger logger, Exception ex, params (string key, string value)[] properties) =>
+            Log(logger, ex, GetProperties(properties));
+
         public static void Log(this ILogger logger, Exception ex, Category category, IDictionary<string, string> properties)
         {
             if (properties == null)
@@ -57,11 +66,20 @@ namespace Prism.Logging
             logger.Log(ex, properties);
         }
 
+        public static void Log(this ILogger logger, Exception ex, Category category, params (string key, string value)[] properties) =>
+            Log(logger, ex, category, GetProperties(properties));
+
         public static void Report(this ICrashesService logger, Exception ex) =>
             logger.Report(ex, new Dictionary<string, string>());
 
+        public static void Report(this ICrashesService logger, Exception ex, params (string key, string value)[] properties) =>
+            logger.Report(ex, GetProperties(properties));
+
         public static void TrackEvent(this IAnalyticsService logger, string name) =>
             logger.TrackEvent(name, new Dictionary<string, string>());
+
+        public static void TrackEvent(this IAnalyticsService logger, string name, params (string key, string value)[] properties) =>
+            logger.TrackEvent(name, GetProperties(properties));
 
         public static void Warn(this ILogger logger, string message, IDictionary<string, string> properties = null)
         {
@@ -73,6 +91,19 @@ namespace Prism.Logging
             properties[nameof(Category)] = $"{Category.Warn}";
 
             logger.Log(message, properties);
+        }
+
+        public static void Warn(this ILogger logger, string message, params (string key, string message)[] properties) =>
+            Warn(logger, message, GetProperties(properties));
+
+        private static IDictionary<string, string> GetProperties((string key, string message)[] properties)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach(var (key, message) in properties)
+            {
+                dict.Add(key, message);
+            }
+            return dict;
         }
     }
 }
