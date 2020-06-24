b91c217efd7b75f126f16cf6994e636b3bd33f66
Dan Siegel <me@dansiegel.net>
2020-06-24 15:45:39 -0700

    cache and retry fixes #1

--- src/Prism.Plugin.Logging.Common/Sockets/SocketMessenger.cs
+++ src/Prism.Plugin.Logging.Common/Sockets/SocketMessenger.cs
@@ -1,53 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using Prism.Logging.Extensions;
+#if !NETSTANDARD
+using Xamarin.Essentials;
+#endif
 
 namespace Prism.Logging.Sockets
 {
     public class SocketMessenger : ISocketMessenger
     {
-        
+#if NETSTANDARD
+        private static readonly string LogCacheFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Prism", "Logging", "Cache", "log");
+#else
+        private static readonly string LogCacheFile = Path.Combine(FileSystem.AppDataDirectory, "Prism", "Logging", "Cache", "log");
+#endif
         internal const int MaxBufferSize = 65500;
 
         public async Task<bool> SendMessageAsync(ILogMessage message, string hostOrIp, int port)
         {
             try
             {
-                var endpoint = GetEndPoint(hostOrIp, port);
-
-                if(endpoint == null)
-                {
-                    return false;
-                }
-
-                using(var socket = new Socket(endpoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp))
-                {
-                    await socket.ConnectAsync(endpoint);
-                    if(!socket.Connected)
-                    {
-                        return false;
-                    }
-
-                    var data = message.GetBytes();
-
-                    if(data.Length > socket.SendBufferSize)
-                    {
-                        Console.WriteLine($"Max Buffer Size {socket.SendBufferSize}. Data packets length is currently: {data.Length}");
-                        Console.WriteLine(message);
-
-                        // HACK: To ensure that the data packet fits in the buffer size
-                        data = data.SubArray(socket.SendBufferSize);
-                    }
-
-                    await socket.SendToAsync(data.ToArraySegment(), SocketFlags.None, endpoint);
-                    return true;
-                }
-
+                return await SendMessageInternalAsync(message, hostOrIp, port);
             }
             catch(SocketException se)
             {
@@ -64,24 +45,71 @@ namespace Prism.Logging.Sockets
 #endif
         }
 
+        private async Task<bool> SendMessageInternalAsync(ILogMessage message, string hostOrIp, int port)
+        {
+            var endpoint = GetEndPoint(hostOrIp, port);
+
+            if (endpoint == null)
+            {
+                
+                return false;
+            }
+
+            var logs = GetLogs(message);
+            using var socket = new Socket(endpoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            await socket.ConnectAsync(endpoint);
+            if (!socket.Connected)
+            {
+                return false;
+            }
+
+            while(logs.Any())
+            {
+                var log = logs.Dequeue();
+                var data = log.GetBytes();
+
+                if (data.Length > socket.SendBufferSize)
+                {
+                    Console.WriteLine($"Max Buffer Size {socket.SendBufferSize}. Data packets length is currently: {data.Length}");
+                    Console.WriteLine(message);
+
+                    // HACK: To ensure that the data packet fits in the buffer size
+                    data = data.SubArray(socket.SendBufferSize);
+                }
+
+                await socket.SendToAsync(data.ToArraySegment(), SocketFlags.None, endpoint);
+                SaveCache(logs);
+            }
+
+            return true;
+        }
+
         public bool SendMessage(ILogMessage message, string hostOrIp, int port)
         {
             try
             {
                 var endpoint = GetEndPoint(hostOrIp, port);
-                using(var socket = new Socket(endpoint.AddressFamily, GetSocketType(), GetProtocolType()))
+                using var socket = new Socket(endpoint.AddressFamily, GetSocketType(), GetProtocolType());
+                var logs = GetLogs(message);
+                socket.Connect(endpoint);
+                if (!socket.Connected)
+                    return false;
+
+                while(logs.Any())
                 {
-                    var data = message.GetBytes();
-                    
-                    if(data.Length > socket.SendBufferSize)
+                    var log = logs.Dequeue();
+                    var data = log.GetBytes();
+
+                    if (data.Length > socket.SendBufferSize)
                     {
-                        Console.WriteLine(message);
+                        Console.WriteLine(log);
                         data = data.SubArray(socket.SendBufferSize);
                     }
-                    
+
                     socket.SendTo(data, endpoint);
-                    return true;
+                    SaveCache(logs);
                 }
+                return true;
             }
             catch(SocketException se)
             {
@@ -120,6 +148,33 @@ namespace Prism.Logging.Sockets
             int chunkSize = (int)(messageSize/((double)MaxBufferSize - prefixSize));
 
             return message.Chunkify(chunkSize);
+        }
+
+        private static void SaveCache(Queue<ILogMessage> queue)
+        {
+            var cacheFile = new FileInfo(LogCacheFile);
+            var formatter = new BinaryFormatter();
+            using var stream = cacheFile.Exists ? cacheFile.OpenWrite() : cacheFile.Create();
+            formatter.Serialize(stream, queue);
+            stream.Close();
+        }
+        private static Queue<ILogMessage> ReadCache()
+        {
+            var cacheFile = new FileInfo(LogCacheFile);
+            var formatter = new BinaryFormatter();
+            using var stream = cacheFile.Exists ? cacheFile.OpenWrite() : cacheFile.Create();
+            var cache = (Queue<ILogMessage>)formatter.Deserialize(stream);
+            stream.Close();
+            return cache;
+        }
+
+        private static Queue<ILogMessage> GetLogs(ILogMessage currentMessage)
+        {
+            var cacheFile = new FileInfo(LogCacheFile);
+            Queue<ILogMessage> cache = cacheFile.Exists ? ReadCache() : new Queue<ILogMessage>();
+            cache.Enqueue(currentMessage);
+            SaveCache(cache);
+            return cache;
         }
     }
 }
